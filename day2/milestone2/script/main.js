9dc763524e165f937754c2c19eaa465c5a8d021e
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-03-17 11:57:07 +0100

    feat: add script for index day2 milestone2

---
+++ day2/milestone2/script/main.js
@@ -0,0 +1,63 @@
+// get element card-container
+const cardContainerElement = document.querySelector('.card-container');
+console.log(cardContainerElement);
+
+// get element error-msg
+const errorMessageElement = document.getElementById('error-msg');
+console.log(errorMessageElement);
+
+// name of the server we wanna reach
+const uri = 'https://lanciweb.github.io/demo/api/pictures/';
+
+// send a request to api
+axios.get(uri).then(response => {
+
+    // get the data formatted in JSON
+    const imgDetails = response.data;
+    console.log(imgDetails);
+
+    // cycle to scroll through the array
+    imgDetails.forEach(element => {
+        
+        console.log(element);
+        // add into card-container the card with the data of current picture
+        cardContainerElement.innerHTML += `
+            <!-- card -->
+            <div class="card">
+                <!-- card image -->
+                <div class="container-image">
+                    <img src="${element.url}" alt="${element.title}">
+                </div>
+                <!-- card description -->
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <!-- card pin image -->
+                <img src="/img/pin.svg" alt="pin" class="pin-card">
+            </div>`;
+    });
+
+}).catch(error => {
+
+    // get the status(number error) of the response
+    const errorStatus = error.response.status;
+    console.log(errorStatus);
+
+    // remove the class hidden-element
+    errorMessageElement.classList.remove('hidden-element');
+
+    // add into error-msg the message with the error
+    errorMessageElement.innerHTML = `Error ${errorStatus}`;
+})
+
+
+// sample card
+/*<!-- card -->
+<div class="card">
+    <!-- card image -->
+    <div class="container-image">
+        <img src="/img/prova1.jpg" alt="prova1">
+    </div>
+    <!-- card description -->
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    <!-- card pin image -->
+    <img src="/img/pin.svg" alt="pin" class="pin-card">
+</div>*/
